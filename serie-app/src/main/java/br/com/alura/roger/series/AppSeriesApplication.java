fb8e8f4b01330d1504fbe402971c942545cee50f
Roger Silva <rogersilvati@gmail.com>
2024-01-22 15:58:38 -0300

    Iniciando curso Java Web: Crie Aplicações usando Spring Boot

---
+++ serie-app/src/main/java/br/com/alura/roger/series/AppSeriesApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.roger.series;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSeriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSeriesApplication.class, args);
+	}
+
+}
---
+++ serie-app/src/test/java/br/com/alura/roger/series/AppSeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.roger.series;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSeriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
