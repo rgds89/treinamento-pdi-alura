fb95f0ca0ca4e5ff60f539edc6997f32071623fa
Roger Silva <rogersilvati@gmail.com>
2024-02-12 09:06:33 -0300

    Refatoracao

--- serie-app/src/main/java/br/com/alura/roger/series/principal/Principal.java
+++ serie-app/src/main/java/br/com/alura/roger/series/principal/Principal.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.util.Locale;
 import java.util.Scanner;
 
 @Component
