5d4452d59d899936251042af9bc6bd54a8f31e9d
Roger Silva <rogersilvati@gmail.com>
2024-02-12 10:00:39 -0300

    Starting Fipe project

---
+++ fipe-table/src/main/java/br/com/alura/roger/fipetable/FipeTableApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.roger.fipetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipeTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipeTableApplication.class, args);
+	}
+
+}
---
+++ fipe-table/src/test/java/br/com/alura/roger/fipetable/FipeTableApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.roger.fipetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
