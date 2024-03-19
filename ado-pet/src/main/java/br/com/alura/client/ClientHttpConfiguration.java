e2694e702cc0040f73aaf217bb8543c20c3993c9
Roger Silva <rogersilvati@gmail.com>
2024-03-19 17:26:18 -0300

    Refatorado cada acao para seu respectivo servico

---
+++ ado-pet/src/main/java/br/com/alura/client/ClientHttpConfiguration.java
@@ -0,0 +1,26 @@
+package br.com.alura.client;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ClientHttpConfiguration {
+    HttpClient client = HttpClient.newHttpClient();
+    public HttpResponse<String> dispararRequisicaoGet(String uri) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+    public HttpResponse<String> dispararRequisicaoPost(String uri, String json) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .header("Content-Type", "application/json")
+                .method("POST", HttpRequest.BodyPublishers.ofString(json))
+                .build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+}
