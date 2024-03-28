2432451097e36e7caed391b629020203efa3e42c
Roger Silva <rogersilvati@gmail.com>
2024-03-28 14:50:44 -0300

    Refatorado entidades

--- ado-pet-api/src/main/java/br/com/alura/adopet/api/model/Adocao.java
+++ ado-pet-api/src/main/java/br/com/alura/adopet/api/model/Adocao.java
@@ -1,45 +1,29 @@
 package br.com.alura.adopet.api.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
 @Table(name = "adocoes")
+@NoArgsConstructor
+@Data
 public class Adocao {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
-
-    @Column(name = "data")
     private LocalDateTime data;
-
-
-    @ManyToOne
-    @JsonBackReference("tutor_adocoes")
-    @JoinColumn(name = "tutor_id")
+    @ManyToOne(fetch = FetchType.LAZY)
     private Tutor tutor;
-
-
-    @OneToOne
-    @JoinColumn(name = "pet_id")
-    @JsonManagedReference("adocao_pets")
+    @OneToOne(fetch = FetchType.LAZY)
     private Pet pet;
-
-
-    @Column(name = "motivo")
     private String motivo;
-
     @Enumerated(EnumType.STRING)
-    @Column(name = "status")
     private StatusAdocao status;
-
-    @Column(name = "justificativa_status")
     private String justificativaStatus;
 
     @Override
@@ -53,61 +37,5 @@ public class Adocao {
     @Override
     public int hashCode() {
         return Objects.hash(id);
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getData() {
-        return data;
-    }
-
-    public void setData(LocalDateTime data) {
-        this.data = data;
-    }
-
-    public Tutor getTutor() {
-        return tutor;
-    }
-
-    public void setTutor(Tutor tutor) {
-        this.tutor = tutor;
-    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
-    }
-
-    public String getMotivo() {
-        return motivo;
-    }
-
-    public void setMotivo(String motivo) {
-        this.motivo = motivo;
-    }
-
-    public StatusAdocao getStatus() {
-        return status;
-    }
-
-    public void setStatus(StatusAdocao status) {
-        this.status = status;
-    }
-
-    public String getJustificativaStatus() {
-        return justificativaStatus;
-    }
-
-    public void setJustificativaStatus(String justificativaStatus) {
-        this.justificativaStatus = justificativaStatus;
     }
 }
