53717ba259a40d50506474e7744239231b969dba
Roger Silva <rogersilvati@gmail.com>
2024-04-04 10:02:37 -0300

    Criado mais cenarios e corrigido bug

--- ado-pet-api/src/main/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoService.java
+++ ado-pet-api/src/main/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoService.java
@@ -1,6 +1,7 @@
 package br.com.alura.adopet.api.service;
 
 import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.TipoPet;
 import br.com.alura.adopet.api.model.enums.ProbabilidadeAdocao;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,7 @@ public class CalculadoraProbabilidadeAdocaoService {
 
     private int calcularNota(Pet pet) {
         int nota = 10;
-        nota = pet.getTipo().equals("CACHORRO") && pet.getPeso().intValue() > 15 ? nota - 2 : pet.getTipo().equals("GATO") && pet.getPeso().intValue() > 10 ? nota - 2 : nota;
+        nota = TipoPet.CACHORRO.equals(pet.getTipo()) && pet.getPeso().intValue() > 15 ? nota - 2 : TipoPet.GATO.equals(pet.getTipo()) && pet.getPeso().intValue() > 10 ? nota - 2 : nota;
         nota = pet.getIdade() >= 15 ? nota - 5 : pet.getIdade() >= 10 ? nota - 4 : nota;
 
         return nota;
--- ado-pet-api/src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoServiceTest.java
+++ ado-pet-api/src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoServiceTest.java
@@ -11,7 +11,7 @@ class CalculadoraProbabilidadeAdocaoServiceTest {
     private Pet pet;
 
     @Test
-    void deveRetornarProbabilidadeAltaParaPetComNotaMaiorOuIgualA8() {
+    void deveRetornarProbabilidadeAltaParaPetComNotaMaiorOuIgualA8TipoPetCachorro() {
         CalculadoraProbabilidadeAdocaoService calculadoraProbabilidadeAdocaoService = new CalculadoraProbabilidadeAdocaoService();
         pet = new Pet();
         pet.setTipo(TipoPet.CACHORRO);
@@ -19,6 +19,61 @@ class CalculadoraProbabilidadeAdocaoServiceTest {
         pet.setIdade(4);
 
         assertEquals(ProbabilidadeAdocao.ALTA, calculadoraProbabilidadeAdocaoService.calcularProbabilidadeAdocao(pet));
+    }
+
+    @Test
+    void deveRetornarProbabilidadeMediaParaPetComNotaMaiorOuIgualA5EMenorQue8TipoPetCachorro() {
+        CalculadoraProbabilidadeAdocaoService calculadoraProbabilidadeAdocaoService = new CalculadoraProbabilidadeAdocaoService();
+        pet = new Pet();
+        pet.setTipo(TipoPet.CACHORRO);
+        pet.setPeso(4.0F);
+        pet.setIdade(15);
+
+        assertEquals(ProbabilidadeAdocao.MEDIA, calculadoraProbabilidadeAdocaoService.calcularProbabilidadeAdocao(pet));
+    }
+
+    @Test
+    void deveRetornarProbabilidadeBaixaParaPetComNotaMenorQue5TipoPetCachorro() {
+        CalculadoraProbabilidadeAdocaoService calculadoraProbabilidadeAdocaoService = new CalculadoraProbabilidadeAdocaoService();
+        pet = new Pet();
+        pet.setTipo(TipoPet.CACHORRO);
+        pet.setPeso(20.0F);
+        pet.setIdade(16);
+
+        assertEquals(ProbabilidadeAdocao.BAIXA, calculadoraProbabilidadeAdocaoService.calcularProbabilidadeAdocao(pet));
+    }
+
+    @Test
+    void deveRetornarProbabilidadeAltaParaPetComNotaMaiorOuIgualA8TipoPetGato() {
+        CalculadoraProbabilidadeAdocaoService calculadoraProbabilidadeAdocaoService = new CalculadoraProbabilidadeAdocaoService();
+        pet = new Pet();
+        pet.setTipo(TipoPet.GATO);
+        pet.setPeso(4.0F);
+        pet.setIdade(4);
+
+        assertEquals(ProbabilidadeAdocao.ALTA, calculadoraProbabilidadeAdocaoService.calcularProbabilidadeAdocao(pet));
+    }
+
+    @Test
+    void deveRetornarProbabilidadeMediaParaPetComNotaMaiorOuIgualA5EMenorQue8TipoPetGato() {
+        CalculadoraProbabilidadeAdocaoService calculadoraProbabilidadeAdocaoService = new CalculadoraProbabilidadeAdocaoService();
+        pet = new Pet();
+        pet.setTipo(TipoPet.GATO);
+        pet.setPeso(4.0F);
+        pet.setIdade(15);
+
+        assertEquals(ProbabilidadeAdocao.MEDIA, calculadoraProbabilidadeAdocaoService.calcularProbabilidadeAdocao(pet));
+    }
+
+    @Test
+    void deveRetornarProbabilidadeBaixaParaPetComNotaMenorQue5TipoPetGato() {
+        CalculadoraProbabilidadeAdocaoService calculadoraProbabilidadeAdocaoService = new CalculadoraProbabilidadeAdocaoService();
+        pet = new Pet();
+        pet.setTipo(TipoPet.GATO);
+        pet.setPeso(20.0F);
+        pet.setIdade(16);
+
+        assertEquals(ProbabilidadeAdocao.BAIXA, calculadoraProbabilidadeAdocaoService.calcularProbabilidadeAdocao(pet));
     }
 
 }
