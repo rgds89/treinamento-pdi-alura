b6df5645bd475f4bc6f0cb1ea8276be5e7b072f1
Roger Silva <rogersilvati@gmail.com>
2024-07-15 15:12:00 -0300

    Created decision making configuration for new group or loss of offset

--- ecommerce/service-email-new-order/src/main/java/br/com/alura/ecommerce/EmailNewOrderService.java
+++ ecommerce/service-email-new-order/src/main/java/br/com/alura/ecommerce/EmailNewOrderService.java
@@ -1,36 +1,32 @@
 package br.com.alura.ecommerce;
 
-import br.com.alura.ecommerce.consumer.KafkaService;
+import br.com.alura.ecommerce.consumer.ConsumerService;
+import br.com.alura.ecommerce.consumer.ServiceRunner;
 import br.com.alura.ecommerce.dispatcher.KafkaDispatcher;
 import br.com.alura.ecommerce.message.Message;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import java.math.BigDecimal;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-public class EmailNewOrderService {
-
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var emailService = new EmailNewOrderService();
-        try (var service = new KafkaService<>(EmailNewOrderService.class.getSimpleName(),
-                "ECOMMERCE_NEW_ORDER",
-                emailService::parse,
-                Map.of())) {
-            service.run();
-        }
-    }
+public class EmailNewOrderService implements ConsumerService<Order> {
 
     private final KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<>();
+    private static final Integer THREADS = 5;
+    private static final String TOPIC_NEW_ORDER = "ECOMMERCE_NEW_ORDER";
+    private static final String TOPIC_EMAIL = "ECOMMERCE_SEND_EMAIL";
 
-    private void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) {
+        new ServiceRunner<>(EmailNewOrderService::new).start(THREADS);
+    }
+
+    public void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
         System.out.println("------------------------------------------");
         System.out.println("Processing new order, preparing email");
         System.out.println(record.value());
 
         var order = record.value().getPayload();
         var emailCode = "Thank you for your order! We are processing your order!";
-        emailDispatcher.send("ECOMMERCE_SEND_EMAIL",
+        emailDispatcher.send(TOPIC_EMAIL,
                 order.getEmail(),
                 emailCode,
                 record.value().getId().continueWith(EmailNewOrderService.class.getSimpleName()));
@@ -38,8 +34,12 @@ public class EmailNewOrderService {
 
     }
 
-    private boolean isFraud(Order order) {
-        return order.getAmount().compareTo(new BigDecimal("4500")) >= 0;
+    public String getTopic() {
+        return TOPIC_NEW_ORDER;
     }
 
+    @Override
+    public String getConsumerGroup() {
+        return EmailNewOrderService.class.getSimpleName();
+    }
 }
