96aed14cac9841800552fc8152377cdb6e5611e2
Roger Silva <rogersilvati@gmail.com>
2024-07-17 00:37:12 -0300

    Refactored frauds service

--- ecommerce/service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ ecommerce/service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -2,17 +2,25 @@ package br.com.alura.ecommerce;
 
 import br.com.alura.ecommerce.consumer.ConsumerService;
 import br.com.alura.ecommerce.consumer.ServiceRunner;
+import br.com.alura.ecommerce.database.LocalDataBase;
 import br.com.alura.ecommerce.dispatcher.KafkaDispatcher;
 import br.com.alura.ecommerce.message.Message;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import java.math.BigDecimal;
+import java.sql.SQLException;
 import java.util.concurrent.ExecutionException;
 
 public class FraudDetectorService implements ConsumerService<Order> {
 
     private static final String TOPIC_ECOMMERCE_NEW_ORDER = "ECOMMERCE_NEW_ORDER";
     private static final int THREADS = 5;
+    private final LocalDataBase dataBase;
+
+    FraudDetectorService() {
+        this.dataBase = new LocalDataBase("frauds_database");
+        this.dataBase.createIfNotExists("create table oders (uuid varchar(200) primary key, is_fraud boolean)");
+    }
 
     public static void main(String[] args) {
         new ServiceRunner<>(FraudDetectorService::new).start(THREADS);
@@ -20,7 +28,7 @@ public class FraudDetectorService implements ConsumerService<Order> {
 
     private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();
 
-    public void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
+    public void parse(ConsumerRecord<String, Message<Order>> record) throws Exception {
         System.out.println("------------------------------------------");
         System.out.println("Processing new order, checking for fraud");
         System.out.println(record.key());
@@ -35,15 +43,28 @@ public class FraudDetectorService implements ConsumerService<Order> {
         }
         var message = record.value();
         var order = message.getPayload();
+
+        if (wasProcessed(order)) {
+            System.out.println("Order " + order.getOrderId() + " was already processed");
+            return;
+        }
+
         if (isFraud(order)) {
+            dataBase.update("insert into frauds (uuid, is_fraud) values (?, true)", order.getOrderId());
             // pretending that the fraud happens when the amount is >= 4500
             System.out.println("Order is a fraud!!!!!" + order);
             orderDispatcher.send("ECOMMERCE_ORDER_REJECTED", order.getEmail(), order, message.getId().continueWith(FraudDetectorService.class.getSimpleName()));
         } else {
+            dataBase.update("insert into orders (uuid, is_fraud) values (?, false)", order.getOrderId());
             System.out.println("Approved: " + order);
             orderDispatcher.send("ECOMMERCE_ORDER_APPROVED", order.getEmail(), order, message.getId().continueWith(FraudDetectorService.class.getSimpleName()));
         }
 
+    }
+
+    private boolean wasProcessed(Order order) throws SQLException {
+        var result = dataBase.query("select uuid from orders where uuid = ? limit 1", order.getOrderId());
+        return result.next();
     }
 
     private boolean isFraud(Order order) {
